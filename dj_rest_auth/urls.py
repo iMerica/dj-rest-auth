5aca84d304e14a4146ef702654acdf042aa96196
Mahmoud Adel <mahmoud.adel.mk@outlook.com>
2020-05-20 01:06:05 +0200

    add url for password_reset_confirm_token_verify

--- dj_rest_auth/urls.py
+++ dj_rest_auth/urls.py
@@ -1,5 +1,5 @@
 from dj_rest_auth.views import (LoginView, LogoutView, PasswordChangeView,
-                                PasswordResetConfirmView, PasswordResetView,
+                                PasswordResetConfirmView, PasswordResetView, PasswordResetConfirmTokenVerifyView,
                                 UserDetailsView)
 from django.conf.urls import url
 from django.conf import settings
@@ -10,6 +10,8 @@ urlpatterns = [
         name='rest_password_reset'),
     url(r'^password/reset/confirm/$', PasswordResetConfirmView.as_view(),
         name='rest_password_reset_confirm'),
+    url(r'^password/reset/confirm/token/verify/$', PasswordResetConfirmTokenVerifyView.as_view(),
+        name='rest_password_reset_confirm_token_verify'),
     url(r'^login/$', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
     url(r'^logout/$', LogoutView.as_view(), name='rest_logout'),
