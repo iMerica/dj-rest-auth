77f3f9834abf80e4aa76518a76c210276292f9d6
Gaurav Sheni <gvsheni@gmail.com>
2020-05-29 22:48:43 -0400

    Update serializers.py

--- dj_rest_auth/serializers.py
+++ dj_rest_auth/serializers.py
@@ -214,7 +214,7 @@ class PasswordResetConfirmSerializer(serializers.Serializer):
 
         # Decode the uidb64 to uid to get User object
         try:
-            uid = url_str_to_user_pk(attrs['uid'])
+            uid = force_text(url_str_to_user_pk(attrs['uid']))
             self.user = UserModel._default_manager.get(pk=uid)
         except (TypeError, ValueError, OverflowError, UserModel.DoesNotExist):
             raise ValidationError({'uid': ['Invalid value']})
