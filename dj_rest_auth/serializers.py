6b636f20626068635f3363db2a13cfdbdf5db6c7
Gaurav Sheni <gvsheni@gmail.com>
2020-06-07 11:44:32 -0400

    Update dj_rest_auth/serializers.py
    Co-authored-by: Michael Gecht <github@gecht.net>

--- dj_rest_auth/serializers.py
+++ dj_rest_auth/serializers.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model
 from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 from django.utils.encoding import force_text
-from django.utils.http import urlsafe_base64_decode as uid_decoder
 from django.utils.module_loading import import_string
 try:
     from django.utils.translation import gettext_lazy as _
