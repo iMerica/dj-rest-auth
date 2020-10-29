8008a982a573e8d6910d9513fc2ac5c2ca179cd9
Joel Whitaker <joel@wolfbyte.co.uk>
2020-10-29 15:05:02 +0000

    Fix import and allow the JWTSerializerWithExpiration to be overridable

--- dj_rest_auth/serializers.py
+++ dj_rest_auth/serializers.py
@@ -162,6 +162,9 @@ class JWTSerializer(serializers.Serializer):
 
 
 class JWTSerializerWithExpiration(JWTSerializer):
+    """
+    Serializer for JWT authentication with expiration times.
+    """
     access_token_expiration = serializers.DateTimeField()
     refresh_token_expiration = serializers.DateTimeField()
 
