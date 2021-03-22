82104dc0370e11de4f85d80dd3eaa7d8d684e050
mos <mostafakhaki00@gmail.com>
2021-03-22 20:08:54 +0430

    Add exception handler for set_callback_url

--- dj_rest_auth/registration/serializers.py
+++ dj_rest_auth/registration/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.http import HttpRequest
+from django.urls.exceptions import NoReverseMatch
 from django.utils.translation import gettext_lazy as _
 from requests.exceptions import HTTPError
 from rest_framework import serializers
@@ -64,11 +65,12 @@ class SocialLoginSerializer(serializers.Serializer):
         self.callback_url = getattr(view, 'callback_url', None)
         if not self.callback_url:
             # auto generate base on adapter and request
-            self.callback_url = reverse(
-                viewname=adapter_class.provider_id + '_callback',
-                request=self._get_request()
-            )
-            if not self.callback_url:
+            try:
+                self.callback_url = reverse(
+                    viewname=adapter_class.provider_id + '_callback',
+                    request=self._get_request()
+                )
+            except NoReverseMatch:
                 raise serializers.ValidationError(
                     _("Define callback_url in view")
                 )
