c825f574cbc5ad5dbbf8494ab2e652ff2996732e
Özer S <oesah88@gmail.com>
2022-01-07 18:01:54 +0100

    feat: implement REST_AUTH_USE_SITES_DOMAIN

--- dj_rest_auth/forms.py
+++ dj_rest_auth/forms.py
@@ -43,7 +43,11 @@ class AllAuthPasswordResetForm(DefaultPasswordResetForm):
                 'password_reset_confirm',
                 args=[user_pk_to_url_str(user), temp_key],
             )
-            url = build_absolute_uri(request, path)
+
+            if settings.REST_AUTH_PW_RESET_USE_SITES_DOMAIN is True:
+                url = build_absolute_uri(None, path)
+            else:
+                url = build_absolute_uri(request, path)
 
             context = {
                 'current_site': current_site,
