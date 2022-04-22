1b1cff410ea651cf3b84f9436b038c45a7e3fde9
Sepehr Hozzari <sepehrhozzari99@gmail.com>
2022-04-22 23:21:16 +0430

    Fix Bug -  Wrong Naming for password reset confirm url

--- dj_rest_auth/urls.py
+++ dj_rest_auth/urls.py
@@ -10,7 +10,7 @@ from dj_rest_auth.views import (
 urlpatterns = [
     # URLs that do not require a session or valid token
     path('password/reset/', PasswordResetView.as_view(), name='rest_password_reset'),
-    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='rest_password_reset_confirm'),
+    path('password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('login/', LoginView.as_view(), name='rest_login'),
     # URLs that require a user to be logged in with a valid session / token.
     path('logout/', LogoutView.as_view(), name='rest_logout'),
