594bf26f92ddaddf5268743ac85199de18ae2560
jazimmerman <jaz.zimms@gmail.com>
2023-06-20 11:23:08 -0400

    added MS graph login option.

--- dj_rest_auth/social_serializers.py
+++ dj_rest_auth/social_serializers.py
@@ -10,7 +10,7 @@ if 'allauth.socialaccount' in settings.INSTALLED_APPS:
     from allauth.socialaccount.models import SocialToken
     from allauth.socialaccount.providers.oauth.client import OAuthError
 
-    from dj_rest_auth.registration.serializers import SocialConnectMixin, SocialLoginSerializer
+    from dj_rest_auth.registration.serializers import SocialConnectMixin
 
 
 class TwitterLoginSerializer(serializers.Serializer):
