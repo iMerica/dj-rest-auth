b01027ced7e16b81183696afd1f20513c3c44bcd
suspiciousRaccoon <127566947+suspiciousRaccoon@users.noreply.github.com>
2023-12-14 18:24:18 -0300

    Add missing allauth middleware to demo settings.py

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware'
 )
 
 # For backwards compatibility for Django 1.8
