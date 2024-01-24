ee69376eb468ff8aabaa1e4043a88fcdad419502
Raoul George <raoulgeorge@gmail.com>
2024-01-24 10:30:17 -0800

    fix: add allauth to middleware
    when the demo is run using
    `python manage.py migrate --settings=demo.settings --noinput`
    a `django.core.exceptions.ImproperlyConfigured` exception is thrown

--- demo/demo/settings.py
+++ demo/demo/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE = (
+    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
