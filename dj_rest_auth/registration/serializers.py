7eb2ec914a4c76d6699c15f8fb9499ae88bbcc66
Raymond Penners <raymond.penners@intenct.nl>
2024-05-02 18:57:48 +0200

    fix: Handle client signature change

--- dj_rest_auth/registration/serializers.py
+++ dj_rest_auth/registration/serializers.py
@@ -127,7 +127,6 @@ class SocialLoginSerializer(serializers.Serializer):
                 adapter.access_token_method,
                 adapter.access_token_url,
                 self.callback_url,
-                scope,
                 scope_delimiter=adapter.scope_delimiter,
                 headers=adapter.headers,
                 basic_auth=adapter.basic_auth,
