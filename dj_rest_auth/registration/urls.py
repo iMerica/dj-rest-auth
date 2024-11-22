737654ae51bf3744a94cb463df2df0c3a5c5858b
Alexandre Augusto <lexander.santos@gmail.com>
2024-11-22 19:24:12 -0300

    Change path to re_path and add ?$ to make trailing slash optional

--- dj_rest_auth/registration/urls.py
+++ dj_rest_auth/registration/urls.py
@@ -6,8 +6,8 @@ from .views import RegisterView, VerifyEmailView, ResendEmailVerificationView
 
 urlpatterns = [
     path('', RegisterView.as_view(), name='rest_register'),
-    path('verify-email/', VerifyEmailView.as_view(), name='rest_verify_email'),
-    path('resend-email/', ResendEmailVerificationView.as_view(), name="rest_resend_email"),
+    re_path(r'verify-email/?$', VerifyEmailView.as_view(), name='rest_verify_email'),
+    re_path(r'resend-email/?$', ResendEmailVerificationView.as_view(), name="rest_resend_email"),
 
     # This url is used by django-allauth and empty TemplateView is
     # defined just to allow reverse() call inside app, for example when email
@@ -24,8 +24,8 @@ urlpatterns = [
         r'^account-confirm-email/(?P<key>[-:\w]+)/$', TemplateView.as_view(),
         name='account_confirm_email',
     ),
-    path(
-        'account-email-verification-sent/', TemplateView.as_view(),
+    re_path(
+        r'account-email-verification-sent/?$', TemplateView.as_view(),
         name='account_email_verification_sent',
     ),
 ]
